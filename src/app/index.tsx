5c50f76f66268862e70fd55d2f93ae30bf8a2e01
Suman Maity <someone.cpp@proton.me>
2026-09-20 23:28:42 +0530

    refactor: remove template boilerplate files and clean up project structure

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,63 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from "react";
+import { Text, View, StyleSheet, Button, Alert, Switch, ScrollView,
+ TextInput, Pressable, Image, 
+ ImageBackground} from "react-native";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+function HomeScreen(){
+  
+  const [isEnable, setisEnable] = useState(false)
+  const toggleSwitch = () => setisEnable((previousState) => !previousState)
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
+  const pressMe = () => {
+    Alert.alert("Button Clicked");
   }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
+  return(
+    <ScrollView>
+      {/* {[...Array(60)].map((_, i) => (
+        <Text key={i} style={{fontSize:20 }}>Hello I am Suman Maity,{i + 1}</Text>
+      ))} */}
+     
+      {/* <Button title="Click Me" onPress={pressMe}/>
+      <Switch
+      value={isEnable}
+      onValueChange={toggleSwitch}
+      /> */}
+
+      {/* <TextInput 
+      keyboardType="email-address"
+
+  style={{ 
+    borderWidth: 1, 
+    borderColor: 'gray', 
+    height: 40, 
+    paddingHorizontal: 10 ,
+  }} 
+  placeholder="Type here..."
+/>
+
+<Pressable onPress={pressMe}>
+  <Text>Hi I am Pressable</Text>
+</Pressable> */}
+
+
+{/* <Image
+source={{
+  uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTulcuAnYpbLBGRJlau2476cXdLU1oIJnDsbmYPeOnLLv861YpLl7lz0ZY&s"
+}}
+style = {{
+  height: 350,
+  width : 350
+}}
+/> */}
+
+<ImageBackground
+source={{
+  uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTulcuAnYpbLBGRJlau2476cXdLU1oIJnDsbmYPeOnLLv861YpLl7lz0ZY&s"
+}}
+/>
+
+    </ScrollView>
+  )
 }
 
-export default function HomeScreen() {
-  return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
+export default HomeScreen;
